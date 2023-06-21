eebdcf18a8f8d5c3e6e652f2daf613d5945817d7
Jamikidu <kangjh1994@gmail.com>
2023-06-21 18:16:08 +0900

    kang

---
+++ kang_project/js/main_js.js
@@ -0,0 +1,39 @@
+window.onload = function(){
+// alert("testing");
+
+// 디지털 시계
+setInterval(mywatch, 1000); //함수 호출
+
+function mywatch(){
+    const date = new Date()
+    let now = date.toLocaleTimeString();
+    document.getElementById("demo").innerHTML = now;
+}   // 시계 함수 끝
+
+
+
+/* 메인사진변경함수 */
+let picture = ["./images/sokcho1.jpg", "./images/sokcho2.jpg", "./images/sokcho3.jpg",
+                "./images/gangneung1.png", "./images/gangneung2.jpg", "./images/gangneung3.jpg" ];
+
+//사진 변경 스크립트 원본
+let imgIdx = 0;
+// console.log(Math.round((Math.random()*picture.length))); 혹시나 랜덤으로 할 경우대비
+// let first_pic = Math.round((Math.random()*picture.length));
+
+
+changePicture();
+
+function changePicture(){
+    let img = document.getElementById("pic1")
+    img.setAttribute("src",picture[imgIdx]);
+    // img.src = picture[first_pic];
+    imgIdx++;
+
+    if(imgIdx == picture.length){
+        imgIdx = 0;
+    }
+
+    setTimeout(changePicture, 3000); //콜백함수
+}
+}   // 사진 변경 함수 끝
